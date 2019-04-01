18df4744917376b491153336dae385672f965a35
mrxy001 <wxy19980615@gmail.com>
2019-04-01 17:23:35 +0800

    修改“我的”界面的文字

--- app/src/main/java/com/iwxyi/letsremember/Fragments/MineFragment.java
+++ app/src/main/java/com/iwxyi/letsremember/Fragments/MineFragment.java
@@ -23,6 +23,6 @@ public class MineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_group, container, false);
+        return inflater.inflate(R.layout.fragment_mine, container, false);
     }
 }
