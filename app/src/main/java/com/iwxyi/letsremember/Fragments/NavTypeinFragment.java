9a183094daed8a7197ab6fb81d7c0bbc072f5003
mrxy001 <wxy19980615@gmail.com>
2019-04-11 16:50:12 +0800

    启动时自动刷新录入框内容

--- app/src/main/java/com/iwxyi/letsremember/Fragments/NavTypeinFragment.java
+++ app/src/main/java/com/iwxyi/letsremember/Fragments/NavTypeinFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,9 +32,12 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
     private Button mBalanceBtn;
     private Button mWithdrawalBtn;
 
-    private String current_pacage;
-    private String current_section;
-    private int current_card;
+    private String current_package; // 当前记忆包名
+    private String current_section; // 当前章节名
+    private int current_card;       // 当前卡片名
+
+    private String cards_left;
+    private String cards_right;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,6 +50,23 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
         View view = inflater.inflate(R.layout.fragment_typein, container, false);
         initView(view);
         return view;
+    }
+
+    private void initView(@NonNull final View itemView) {
+        mPackageSp = (Spinner) itemView.findViewById(R.id.sp_package);
+        mSectionSp = (Spinner) itemView.findViewById(R.id.sp_section);
+        mCardSp = (Spinner) itemView.findViewById(R.id.sp_card);
+        mTypeinEt = (EditText) itemView.findViewById(R.id.et_typein);
+        mMyTypeinBtn = (Button) itemView.findViewById(R.id.btn_my_typein);
+        mMyTypeinBtn.setOnClickListener(this);
+        mIntegralBtn = (Button) itemView.findViewById(R.id.btn_integral);
+        mIntegralBtn.setOnClickListener(this);
+        mBalanceBtn = (Button) itemView.findViewById(R.id.btn_balance);
+        mBalanceBtn.setOnClickListener(this);
+        mWithdrawalBtn = (Button) itemView.findViewById(R.id.btn_withdrawal);
+        mWithdrawalBtn.setOnClickListener(this);
+
+        refreshAllSpinner();
     }
 
     public void refreshAllSpinner() {
@@ -76,7 +95,7 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
         ArrayList<String> package_names = new ArrayList<>();
         File package_dir = new File(Paths.getLocalPath("material"));
         File[] package_files = package_dir.listFiles();
-        for (int i = 0; i < package_files .length; i++) {
+        for (int i = 0; i < package_files.length; i++) {
             package_names.add(package_files[i].getName());
         }
         ArrayAdapter<String> package_adapter = new ArrayAdapter<>
@@ -86,34 +105,34 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
         // 设置当前默认值
         String editing_package = App.getVal("editing_package");
         if (!editing_package.isEmpty() && package_names.contains(editing_package)) {
-            for (int i = 0; i < package_names .size(); i++) {
+            for (int i = 0; i < package_names.size(); i++) {
                 if (editing_package.equals(package_names.get(i))) {
                     mPackageSp.setSelection(i);
                     break;
                 }
             }
-            current_pacage = editing_package;
+            current_package = editing_package;
         } else if (package_names.size() > 0) {
-            current_pacage = package_names.get(0);
+            current_package = package_names.get(0);
         } else {
             Toast.makeText(getContext(), "找不到记忆包，请检查是否禁止了APP的读取存储权限？", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void refreshSectionSpinner() {
-        if (current_pacage.isEmpty()) {
+        if (current_package.isEmpty()) {
             current_section = "";
-            return ;
+            return;
         }
 
         // 设置列表
         ArrayList<String> section_names = new ArrayList<>();
-        File section_dir = new File(Paths.getLocalPath("material/"+current_pacage));
+        File section_dir = new File(Paths.getLocalPath("material/" + current_package));
         File[] section_files = section_dir.listFiles();
-        for (int i = 0; i < section_files .length; i++) {
+        for (int i = 0; i < section_files.length; i++) {
             String fileName = section_files[i].getName();
             if (fileName.endsWith(".txt")) {
-                fileName = fileName.substring(0, fileName.length()-4);
+                fileName = fileName.substring(0, fileName.length() - 4);
             }
             section_names.add(fileName);
         }
@@ -124,7 +143,7 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
         // 设置默认值
         String editing_section = App.getVal("editing_section");
         if (!editing_section.isEmpty() && section_names.contains(editing_section)) {
-            for (int i = 0; i < section_names .size(); i++) {
+            for (int i = 0; i < section_names.size(); i++) {
                 if (editing_section.equals(section_names.get(i))) {
                     mSectionSp.setSelection(i);
                     break;
@@ -139,23 +158,23 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
     public void refreshCardSpinner() {
         if (current_section.isEmpty()) {
             current_card = 0;
-            return ;
+            return;
         }
 
         // 设置列表
         ArrayList<String> card_names = new ArrayList<>();
 
-        String path = "material/" + current_pacage + "/" + current_section + ".txt";
+        String path = "material/" + current_package + "/" + current_section + ".txt";
         String content = FileUtil.readTextVals(path);
         if (content.isEmpty()) {
             App.err("无法读取文件：" + path);
-            return ;
+            return;
         }
-        ArrayList<String> cards = StringUtil .getXmls(content, "card");
-        for (int i = 0; i < cards .size(); i++) {
+        ArrayList<String> cards = StringUtil.getXmls(content, "card");
+        for (int i = 0; i < cards.size(); i++) {
             String title = StringUtil.getXml(cards.get(i), "content");
             title = getContentTitle(title);
-            card_names.add(""+(i+1)+". "+title);
+            card_names.add("" + (i + 1) + ". " + title);
         }
         ArrayAdapter<String> card_adapter = new ArrayAdapter<>
                 (getContext(), android.R.layout.simple_spinner_item, card_names);
@@ -181,21 +200,37 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
         return ans;
     }
 
-    private void initView(@NonNull final View itemView) {
-        mPackageSp = (Spinner) itemView.findViewById(R.id.sp_package);
-        mSectionSp = (Spinner) itemView.findViewById(R.id.sp_section);
-        mCardSp = (Spinner) itemView.findViewById(R.id.sp_card);
-        mTypeinEt = (EditText) itemView.findViewById(R.id.et_typein);
-        mMyTypeinBtn = (Button) itemView.findViewById(R.id.btn_my_typein);
-        mMyTypeinBtn.setOnClickListener(this);
-        mIntegralBtn = (Button) itemView.findViewById(R.id.btn_integral);
-        mIntegralBtn.setOnClickListener(this);
-        mBalanceBtn = (Button) itemView.findViewById(R.id.btn_balance);
-        mBalanceBtn.setOnClickListener(this);
-        mWithdrawalBtn = (Button) itemView.findViewById(R.id.btn_withdrawal);
-        mWithdrawalBtn.setOnClickListener(this);
+    /**
+     * 刷新录入框的内容
+     */
+    public void refreshTypeinContent() {
+        String path = "material/" + current_package + "/" + current_section + ".txt";
+        String content = FileUtil.readTextVals(path);
+        if (content.isEmpty()) {
+            App.err("无法读取文件：" + path);
+            return;
+        }
+        ArrayList<String> cards = StringUtil.getXmls(content, "card");
 
-        refreshAllSpinner();
+        // 判断索引
+        int old_index = current_card;
+        if (current_card < 0) {
+            current_card = 0;
+        }
+        if (current_card >= cards.size()) {
+            current_card = cards.size() - 1;
+        }
+        if (old_index != current_card) {
+            mCardSp.setSelection(current_card);
+        }
+
+        // 设置当前索引
+        String card = cards.get(current_card);
+        String con = StringUtil.getXml(card, "content");
+        mTypeinEt.setText(con);
+
+        // 设置保存的前后文本(增强性能)
+
     }
 
     @Override
