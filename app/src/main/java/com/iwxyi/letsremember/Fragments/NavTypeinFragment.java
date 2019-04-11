335e0bddba6e058b438cfd4685bef3419a6682d3
mrxy001 <wxy19980615@gmail.com>
2019-04-11 16:39:20 +0800

    三个Spinner初始化列表

--- app/src/main/java/com/iwxyi/letsremember/Fragments/NavTypeinFragment.java
+++ app/src/main/java/com/iwxyi/letsremember/Fragments/NavTypeinFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,8 @@ import android.widget.Toast;
 import com.iwxyi.letsremember.Globals.App;
 import com.iwxyi.letsremember.Globals.Paths;
 import com.iwxyi.letsremember.R;
+import com.iwxyi.letsremember.Utils.FileUtil;
+import com.iwxyi.letsremember.Utils.StringUtil;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -32,7 +35,7 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
 
     private String current_pacage;
     private String current_section;
-    private String current_card;
+    private int current_card;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -93,7 +96,7 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
         } else if (package_names.size() > 0) {
             current_pacage = package_names.get(0);
         } else {
-            Toast.makeText(getContext(), "请手动创建记忆包", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "找不到记忆包，请检查是否禁止了APP的读取存储权限？", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -108,7 +111,11 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
         File section_dir = new File(Paths.getLocalPath("material/"+current_pacage));
         File[] section_files = section_dir.listFiles();
         for (int i = 0; i < section_files .length; i++) {
-            section_names.add(section_files[i].getName());
+            String fileName = section_files[i].getName();
+            if (fileName.endsWith(".txt")) {
+                fileName = fileName.substring(0, fileName.length()-4);
+            }
+            section_names.add(fileName);
         }
         ArrayAdapter<String> section_adapter = new ArrayAdapter<>
                 (getContext(), android.R.layout.simple_spinner_item, section_names);
@@ -116,15 +123,62 @@ public class NavTypeinFragment extends Fragment implements View.OnClickListener 
 
         // 设置默认值
         String editing_section = App.getVal("editing_section");
-
+        if (!editing_section.isEmpty() && section_names.contains(editing_section)) {
+            for (int i = 0; i < section_names .size(); i++) {
+                if (editing_section.equals(section_names.get(i))) {
+                    mSectionSp.setSelection(i);
+                    break;
+                }
+            }
+            current_section = editing_section;
+        } else if (section_names.size() > 0) {
+            current_section = section_names.get(0);
+        }
     }
 
     public void refreshCardSpinner() {
         if (current_section.isEmpty()) {
-            current_card = "";
+            current_card = 0;
             return ;
         }
 
+        // 设置列表
+        ArrayList<String> card_names = new ArrayList<>();
+
+        String path = "material/" + current_pacage + "/" + current_section + ".txt";
+        String content = FileUtil.readTextVals(path);
+        if (content.isEmpty()) {
+            App.err("无法读取文件：" + path);
+            return ;
+        }
+        ArrayList<String> cards = StringUtil .getXmls(content, "card");
+        for (int i = 0; i < cards .size(); i++) {
+            String title = StringUtil.getXml(cards.get(i), "content");
+            title = getContentTitle(title);
+            card_names.add(""+(i+1)+". "+title);
+        }
+        ArrayAdapter<String> card_adapter = new ArrayAdapter<>
+                (getContext(), android.R.layout.simple_spinner_item, card_names);
+        mCardSp.setAdapter(card_adapter);
+
+        // 设置默认值
+        int editing_card = App.getInt("editing_card");
+        if (editing_card > 0 && editing_card < card_names.size()) {
+            current_card = editing_card;
+        } else {
+            current_card = 0;
+        }
+    }
+
+    private static String getContentTitle(String c) {
+        String ans = c;
+        int pos = c.indexOf("\n");
+        if (pos > -1) {
+            ans = c.substring(0, pos);
+        }
+        if (ans.length() > 20)
+            ans = ans.substring(0, 20);
+        return ans;
     }
 
     private void initView(@NonNull final View itemView) {
